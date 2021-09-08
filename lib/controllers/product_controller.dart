27e1d919a6d378ef3be855d629144ade3a43be9f
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-09-08 20:26:31 +0600

    Initializing getProducts function on Controller

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -5,6 +5,13 @@ import 'package:get/state_manager.dart';
 class ProductController extends GetxController {
   var isLoading = true.obs;
   var productList = <Product>[].obs;
+
+  @override
+  void onInit() {
+    getProducts();
+    super.onInit();
+  }
+
   getProducts() async {
     try {
       var products = await ApiServices.getProducts();
