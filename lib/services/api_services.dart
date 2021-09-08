0dd4b91dc9d6fe34a720bfd7eacd33ca8130cd89
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-09-08 20:27:17 +0600

    ApiSevice class added to manage all api calls

---
+++ lib/services/api_services.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_rest_api_getx/constants/end_points.dart';
+import 'package:flutter_rest_api_getx/models/Products.dart';
+import 'package:http/http.dart' as http;
+
+class ApiServices {
+}
