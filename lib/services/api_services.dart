d3dfdac01d7d827d99850b5540b7b8bcb84ae16a
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-09-08 20:28:28 +0600

    Getting products from server

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -4,4 +4,19 @@ import 'package:http/http.dart' as http;
 
 class ApiServices {
   static var client = http.Client();
+
+  static Future<List<Product>> getProducts() async {
+    String url = EndPoints.baseUrl + EndPoints.products;
+
+    var response = await client.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      var jsonValue = response.body;
+
+      return productFromJson(jsonValue);
+    } else {
+      List<Product> emptyList = [];
+      return emptyList;
+    }
+  }
 }
