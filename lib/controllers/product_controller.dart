967159655a6d70b63658eb30070e60d11212fa4f
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-09-08 20:24:54 +0600

    Controller Class Added on Controller

---
+++ lib/controllers/product_controller.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_rest_api_getx/models/Products.dart';
+import 'package:flutter_rest_api_getx/services/api_services.dart';
+import 'package:get/state_manager.dart';
+
+class ProductController extends GetxController {
+}
