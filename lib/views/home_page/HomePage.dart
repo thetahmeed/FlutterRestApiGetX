491d44bbc631f24cd8e58e01e6fd75e1bf081b13
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-09-09 21:38:57 +0600

    Displaying products by StaggeredGridView.countBuilder

--- lib/views/home_page/HomePage.dart
+++ lib/views/home_page/HomePage.dart
@@ -12,7 +12,34 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: mAppBar(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Obx(
+                () {
+                  return StaggeredGridView.countBuilder(
+                    physics: BouncingScrollPhysics(),
+                    crossAxisCount: 2,
+                    itemCount: productController.productList.length,
+                    //crossAxisSpacing: 4,
+                    //mainAxisSpacing: 4,
+                    itemBuilder: (context, index) {
+                      Product product = productController.productList[index];
+
+                      return SingleProductModel(product: product);
+                    },
+                    staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+
   AppBar mAppBar() {
     return AppBar(
       backgroundColor: Colors.transparent,
