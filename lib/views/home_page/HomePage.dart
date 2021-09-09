9cf02dcd6de6cca826335082dfa26c019b0aecb1
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-09-09 21:36:44 +0600

    Home Class Created

---
+++ lib/views/home_page/HomePage.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rest_api_getx/controllers/product_controller.dart';
+import 'package:flutter_rest_api_getx/models/Products.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:get/get.dart';
+import 'components/SingleProductModel.dart';
+
+class HomePage extends StatelessWidget {
+}
