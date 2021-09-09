045306f54443316b5873b71b9a0cfa4a58321c7a
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-09-09 21:37:17 +0600

    AppBar

--- lib/views/home_page/HomePage.dart
+++ lib/views/home_page/HomePage.dart
@@ -7,4 +7,31 @@ import 'components/SingleProductModel.dart';
 
 class HomePage extends StatelessWidget {
   final ProductController productController = Get.put(ProductController());
+
+  AppBar mAppBar() {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      title: Text(
+        'mShop',
+        style: TextStyle(
+          color: Colors.blue,
+        ),
+      ),
+      centerTitle: true,
+      elevation: 0,
+      leading: Icon(
+        Icons.arrow_back_ios,
+        color: Colors.blue,
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(
+            Icons.shopping_cart,
+            color: Colors.blue,
+          ),
+          onPressed: () {},
+        )
+      ],
+    );
+  }
 }
